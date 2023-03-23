367b146e6e255d608aac0638c7772808f9201c95
Roman Myasnikov <roman.myasnikov@bostongene.com>
2023-03-23 14:11:57 +0400

    add

--- task.sh
+++ task.sh
@@ -1,9 +1,4 @@
 #!/bin/bash -u
 echo "Starting a python script"
-
-# А ещё получить API ключ и передать его в скрипт
-ls -l
-python hello.py
-
+python kek.py
 echo "Completing a python script"
-# А Ещё кинуть результат работы на почту или куда-то
